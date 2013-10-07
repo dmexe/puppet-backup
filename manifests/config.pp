856bd4637989f4e0dcfb40bc33fae11b5b66b8b0
Dmitry Galinsky <dima.exe@gmail.com>
2013-10-07 17:14:46 +0400

    typo in config

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,7 +3,7 @@ class backup::config (
   $package_version      = '3.7.1',
   $s3_access_key_id     = undef,
   $s3_secret_access_key = undef,
-  $s3_backet_default    = undef,
+  $s3_bucket_default    = undef,
   $s3_region            = 'eu-west-1',
   $local                = undef,
   $command              = '/usr/local/bin/backup'
